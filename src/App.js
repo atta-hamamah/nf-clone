ef10bde0cc5495b6020c1e684b11664f71e8693a
‪atta Hamamah <7atta3@gmail.com>
2023-01-21 15:47:10 +0200

    firstt

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Banner from './Components/Banner';
+import Navbar from './Components/Navbar';
+import Row from './Components/Row';
+import requests from './requests';
+
+
+
+function App() {
+  return (
+    <div className="app">
+
+      <Navbar />
+      
+      <Banner />
+       
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+     
+    </div>
+  );
+}
+
+export default App;
