0761666be134682a22d7ba2039056596ab28e897
‪atta Hamamah <7atta3@gmail.com>
2024-09-15 17:14:34 +0200

    added ESL still deploy

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react';
 import './Row.css';
 import axios from '../axios';
@@ -6,65 +7,65 @@ import movieTrailer from 'movie-trailer'
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title , fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  
+
   const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
-    async function fetchData(){
-     const request = await axios.get(fetchUrl);
-     setMovies(request.data.results);
-     return request;
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     } fetchData();
-    
+
   }, [fetchUrl]);
- 
-   const opts = {
-     height: "390",
-     width: "100%",
-     playerVars: {
-       autoplay:1,
-     },
-   }
-     
-   const handleClick = (movie) => {
-     if(trailerUrl){
-       setTrailerUrl("");
-     }else{
-       movieTrailer(movie?.name || "")
-       .then(url => {
-         const urlParams = new URLSearchParams( new URL(url).search);
-         setTrailerUrl(urlParams.get('v'));
 
-       }).catch(error => console.log(error))
-     }
-       };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  }
 
-    return (
-        <div className="row" >
-            <h2>{title}</h2>
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
 
-            <div className="row__posters" >
+        }).catch(error => console.log(error))
+    }
+  };
 
-              {movies?.map(movie =>(
-                <img 
-                key={movie.id}
-                onClick={()=> handleClick(movie)}
-                className={`row__poster  ${isLargeRow && "row__posterLarge"} `}
-                src={`${baseUrl}${isLargeRow ? movie?.poster_path : movie?.backdrop_path}`}
-                alt={movie.name} 
-                
-                />
-              ))}
+  return (
+    <div className="row" >
+      <h2>{title}</h2>
 
-            </div>
-            {trailerUrl && <YouTube
-             videoId={trailerUrl}  
-             opts={opts}
-            />}
-        </div>
-    )
+      <div className="row__posters" >
+
+        {movies?.map(movie => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster  ${isLargeRow && "row__posterLarge"} `}
+            src={`${baseUrl}${isLargeRow ? movie?.poster_path : movie?.backdrop_path}`}
+            alt={movie.name}
+
+          />
+        ))}
+
+      </div>
+      {trailerUrl && <YouTube
+        videoId={trailerUrl}
+        opts={opts}
+      />}
+    </div>
+  )
 }
 
 export default Row;
